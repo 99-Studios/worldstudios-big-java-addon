d032b5721f0ae1b5b57450e271f568e67cfdadb1
99-Studios <worldstudiosanimations@gmail.com>
2026-08-11 13:51:08 -0700

    Nerf/Buff Super Hunger

--- src/main/java/net/mcreator/worldstudiosworld/procedures/SuperHungerOnEffectActiveTickProcedure.java
+++ src/main/java/net/mcreator/worldstudiosworld/procedures/SuperHungerOnEffectActiveTickProcedure.java
@@ -1,13 +1,17 @@
 package net.mcreator.worldstudiosworld.procedures;
 
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+
+import net.mcreator.worldstudiosworld.init.WorldstudiosWorldModMobEffects;
 
 public class SuperHungerOnEffectActiveTickProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
 		if (entity instanceof Player _player)
-			_player.getFoodData().setFoodLevel((int) ((entity instanceof Player _plr ? _plr.getFoodData().getFoodLevel() : 0) - 0.025));
+			_player.getFoodData().setFoodLevel((int) ((entity instanceof Player _plr ? _plr.getFoodData().getFoodLevel() : 0)
+					- 0.0125 * ((entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(WorldstudiosWorldModMobEffects.SUPER_HUNGER) ? _livEnt.getEffect(WorldstudiosWorldModMobEffects.SUPER_HUNGER).getAmplifier() : 0) + 1)));
 	}
 }
